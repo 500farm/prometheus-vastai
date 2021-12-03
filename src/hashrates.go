2c6f8e0e5594fd7ed11b648e99323359b4fb7d8f
Sergey Cheperis <czz@czz.name>
2021-12-03 17:12:56 +0300

    Add hashrates

--- src/hashrates.go
+++ src/hashrates.go
@@ -13,7 +13,7 @@ var HashRates HashRateMap = HashRateMap{ // MH/s
 	"GTX 1080 Ti": 36,
 	// "Q RTX 5000": ,
 	// "Q RTX 6000": ,
-	// "Q RTX 8000": ,
+	"Q RTX 8000":  50,
 	"RTX 2070":    36,
 	"RTX 2070S":   37,
 	"RTX 2080":    40,
@@ -21,16 +21,33 @@ var HashRates HashRateMap = HashRateMap{ // MH/s
 	// "RTX 3060": ,
 	// "RTX 3060 Ti": ,
 	"RTX 3070": 56,
+	// "RTX 3070 Ti": ,
 	"RTX 3080": 84,
 	// "RTX 3080 Ti": ,
 	"RTX 3090": 106,
 	// "RTX A4000": ,
-	// "RTX A5000": ,
-	// "RTX A6000": ,
+	"RTX A5000": 87,
+	"RTX A6000": 87,
 	// "Tesla K80": ,
 	"Tesla P100": 69,
 	"Tesla T4":   25,
 	"Tesla V100": 94,
 	"Titan RTX":  74,
-	// "Titan Xp": ,
+	"Titan Xp":   75,
 }
+
+/**
+Non-LHR cards:
+	RTX 3090	  Only Full Hash Rate
+LHR-only cards:
+	RTX 3080 Ti	  Only Lite Hash Rate
+	RTX 3070 Ti	  Only Lite Hash Rate
+Varying:
+	RTX 3080	  Full Hash Rate or Lite Hash rate after late May 2021
+	RTX 3070	  Full Hash Rate or Lite Hash rate after late May 2021
+	RTX 3060 Ti	  Full Hash Rate or Lite Hash rate after late May 2021
+	RTX 3060	  Lite Hash Rate or Full Hash Rate for v1 cards with leaked NVIDIA drivers
+
+according to:
+	https://www.nicehash.com/blog/post/how-to-distinguish-between-lhr-and-fhr-graphic-card?lang=en
+*/
